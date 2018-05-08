fbd802882ced820dbee4edd30d0394943fce28a5
Leonn34 <eltonsousadesigner@gmail.com>
2018-05-07 23:32:03 -0400

    update stunnel ufaaa

--- scripts/menu-stunnel4.sh
+++ scripts/menu-stunnel4.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
-####################################
-#CORES:
-#ESTILOS:
-#00: Nenhum
-#01: Negrito
-#04: Sublinhado
-#05: Piscante
-#07: Reverso
-#08: Oculto
-####################################
-#CORES TEXTO:
-#30: preto
-#31: Vermelho
-#32: verde
-#33: amarelo
-#34: Azul
-#35: Magenta (Rosa)
-#36: Ciano (Azul Ciano)
-#37: branco
-####################################
-#CORES FUNDO:
-#40: pretoo
-#41: Vermelho
-#42: verde
-#43: Amarelo
-#44: Azul
-#45: Magenta (Rosa)
-#46: Ciano (Azul Ciano)
-#47: Branco
-###############EXEMPLO:#############
-#Viva o Linux
-#echo -e '\033[01;37mViva o \033[04;32mLinux\033[00;37m!!!'
-#############################################
-##CORES DE FONTES,###################
-##INSERIR A COR NO INÍCIO E FINALIZA COM FIM
-##EXEMPLO:
-## echo -e $amarelo"SEU_TEXTO_AQUI"$fim
+
 #############################################
 corPadrao="\033[0m"
 preto="\033[0;30m"
@@ -56,67 +20,133 @@ cyanClaro="\033[1;36m"
 branco="\033[1;37m"
 fim="\033[0;37m"
 #############################################
+
 #--------------------------------------------
 drop1="/etc/checker/drop_status"
 stun="/etc/checker/stun_status"
 openv="/etc/checker/open_status"
 if [ -d /etc/checker ]; then
-  echo -ne ""
-  else 
-  mkdir /etc/checker
-  fi
+echo -ne ""
+else 
+mkdir /etc/checker
+fi
 #--------------------------------------------
-
+# menu stunnel
+menu-stunnel() {
 clear
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 echo -e $branco" MENU STUNNEL4"$fim
-echo ""
-echo -e $amarelo" [1]"$fim $branco"ATIVAR"$fim
-echo -e $amarelo" [2]"$fim $branco"DESATIVAR"$fim
-echo -e $amarelo" [3]"$fim $branco"SAIR"$fim
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
-read -p " DIGITE SUA OPÇÃO: [1-3] " opcao
-echo -e $cinzaClaro" OPÇÃO ESCOLHIDA:"$fim $branco$opcao$fim
-
-case $opcao in
-
-1)
-clear
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
-echo -e $amarelo" ATIVANDO STUNNEL..."$fim
-#condição verifica se o stunnel está instalado, se não tiver será instalado...
+echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 if [ -d /etc/stunnel ] ; then
-true
+  echo -ne ""
+  ps -C stunnel4 > /dev/null
+    if [ $? = 0 ]; then
+      echo -e $pretoCinza" STATUS:"$fim $verdeClaro"ON"$fim
+      echo "STUNNEL4" > $stun # cria o arquivo pra verificar se está ativo no menu principal
+      echo -e ""
+      echo -e $branco" [1] PARAR"$fim
+      echo -e $branco" [2] SAIR"$fim
+      echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+      read -p " OPÇÃO [1-2] :" opcao
+      case $opcao in
+        1)
+        stun_off
+        exit
+        ;;
+        2)
+        menu
+        exit
+        ;;
+        *)
+        echo -e $vermelhoClaro" OPÇÃO INVÁLIDA!"$fim
+        sleep 3
+        menu-stunnel
+        exit
+        ;;
+        esac
+      else
+        echo -e $pretoCinza" STATUS:"$fim $vermelho"OFF"$fim
+        echo -e ""
+        echo -e "" > $stun # cria o arquivo pra verificar se está ativo no menu principal
+        echo -e $branco" [1] ATIVAR"$fim
+        echo -e $branco" [2] SAIR"$fim
+        echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+        read -p " OPÇÃO [1-2] :" opcao2
+        case $opcao2 in
+          1)
+          stun_on
+          exit
+          ;;
+          2)
+          menu
+          exit
+          ;;
+          *)
+          echo -e $vermelhoClaro" OPÇÃO INVÁLIDA!"$fim
+          sleep 3
+          menu-stunnel
+          exit
+          ;;
+        esac
+      fi
+  else
+    echo -e $vermelhoClaro" STUNNEL4 NÃO INSTALADO!"$fim
+    echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+    read -p " DESEJA INSTALAR? [s/n] :" opcao3
+      if [ -z $opcao3 ]; then
+      echo -e $amarelo" ESCOLHA UMA OPÇÃO!"$fim
+      sleep 3
+      menu-stunnel
+      fi
+      case $opcao3 in
+        (s|S|y|Y)
+        wget https://raw.githubusercontent.com/Leonn34/scriptsousatips/master/scripts/stunnel.sh -O /bin/stunnel4 && chmod +x /bin/stunnel4 && bash /bin/stunnel4
+        exit
+        ;;
+        (n|N)
+        menu
+        exit
+        ;;
+        *)
+        echo -e $vermelhoClaro" OPÇÃO INVÁLIDA!"$fim
+        sleep 3
+        menu-stunnel
+        exit
+        ;;
+      esac
+fi
+}
+# fim menu
+
+stun_on () {
 clear
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
+echo -e $amarelo" ATIVANDO STUNNEL..."$fim
+sleep 3
+clear
 echo -e $amarelo" CONFIGURANDO O SSH..."$fim
-sleep 4
+sleep 3
 clear
 grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
 echo "#Port 443" >> /etc/ssh/sshd_config
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 echo -e $amarelo" REINICIANDO SERVIÇOS..."$fim
-sleep 4
+sleep 3
 clear
 service squid3 stop
 service ssh restart
 service stunnel4 stop
 service stunnel4 start
 clear
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 echo -e $amarelo"SUNNEL ATIVADO!..."$fim
-sleep 5
-echo "STUNNEL4" > $stun
+sleep 3
 clear
-else
-wget https://raw.githubusercontent.com/Leonn34/scripts/master/stunnel.sh && chmod +x stunnel.sh && ./stunnel.sh
-fi
-;;
-2)
+menu-stunnel
+exit
+}
+
+stun_off () {
 clear
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
-echo -e $amarelo"DESATIVANDO STUNNEL4"$fim
-sleep 4
+echo -e $amarelo" DESATIVANDO STUNNEL4"$fim
+sleep 3
 clear
 # REATIVANDO A PORTA 443 NO SSH
 grep -v "^#Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
@@ -127,19 +157,14 @@ service squid3 start
 service stunnel4 stop
 sleep 5
 clear
-echo -e $verdeClaro"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="$fim
 echo -e $amarelo" STUNNEL DESATIVADO COM SUCESSO!"$fim
-echo "" > $stun # checker ativado
-sleep 5
-clear
-menu2
-;;
-3)
-menu2
-;;
-*)
-echo -e $vermelhoClaro"OPÇÃO INVÁLIDA!!!"$fim
 sleep 3
-menu2
+clear
+menu-stunnel
 exit
-esac
+}
+menu-stunnel
+
+
+
+
